ff9652c528064ff816ff16bbfcadf0f9e4bfea4e
cfsonwin <fashu_cheng@163.com>
2022-03-27 00:00:31 +0100

    change on win

--- Django/myboot/myadmin/util.py
+++ Django/myboot/myadmin/util.py
@@ -1,5 +1,8 @@
 import hashlib
 import random
+from datetime import datetime
+
+from django.contrib.gis.geoip2 import GeoIP2
 
 
 def pw_hash(pw):
@@ -17,3 +20,70 @@ def pw_hash_salt(pw, salt):
     new_pass = pw + str(ran_n)  # 'admin123'
     md5.update(new_pass.encode('utf-8'))
     return md5.hexdigest()
+
+
+def get_geo(ip):
+    g = GeoIP2()
+    country = g.country(ip)
+    city = g.city(ip)
+    lat, lon = g.lat_lon(ip)
+    return country, city, lat, lon
+
+
+def get_center_coor(lat1=None, lon1=None, lat2=None, lon2=None):
+    coor = [49.875624225327876, 8.63731024952363]
+    if lat1 and lat2:
+        coor = [(lat1 + lat2) / 2, (lon1 + lon2) / 2]
+    return coor
+
+
+def get_zoom(distance):
+    if distance <= 100:
+        return 8
+    elif distance > 100 and distance <= 5000:
+        return 4
+    else:
+        return 2
+
+
+class Manus:
+    def __init__(self, name, loc, addtime, modify_time, description):
+        self.name = name
+        self.lat = loc.split(',')[0]
+        self.lon = loc.split(',')[1]
+        self.modify_time = modify_time
+        self.addtime = addtime
+        if self.modify_time == self.addtime:
+            self.change_st = 0
+            self.change_msg = 'No Modification'
+        else:
+            self.change_st = 1
+            self.change_msg = self.get_time_diff()
+        self.description = description
+
+    def get_time_diff(self):
+        time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        year_diff = int(time_now.split(' ')[0].split('-')[0]) - int(self.modify_time.split(' ')[0].split('-')[0])
+        month_diff = int(time_now.split(' ')[0].split('-')[1]) - int(self.modify_time.split(' ')[0].split('-')[1])
+        day_diff = int(time_now.split(' ')[0].split('-')[2]) - int(self.modify_time.split(' ')[0].split('-')[2])
+        hour_diff = int(time_now.split(' ')[0].split(':')[0]) - int(self.modify_time.split(' ')[0].split(':')[0])
+        # min_diff = int(time_now.split(' ')[0].split(':')[1]) - int(self.modify_time.split(' ')[0].split(':')[1])
+        # sec_diff = int(time_now.split(' ')[0].split(':')[2]) - int(self.modify_time.split(' ')[0].split(':')[2])
+        if year_diff > 1:
+            msg = 'Last Modification: %d years ago' % year_diff
+        elif year_diff ==1:
+            month_count = year_diff * 12 + month_diff
+            msg = 'Last Modification: %d month ago' % month_count
+        else:
+            if month_diff >= 1:
+                msg = 'Last Modification: %d month ago' % month_diff
+            else:
+                if day_diff > 1:
+                    msg = 'Last Modification: %d days ago' % day_diff
+                elif day_diff == 1:
+                    hour_count = day_diff * 24 + hour_diff
+                    msg = 'Last Modification: %d hours ago' % hour_count
+                else:
+                    msg = 'Last Modification: today'
+
+        return msg
--- Django/myboot/myadmin/views/product.py
+++ Django/myboot/myadmin/views/product.py
@@ -1,10 +1,8 @@
-import os
-import random
-
-from django.http import HttpResponse
+import folium
 from django.shortcuts import render
-from datetime import datetime
+
 from myadmin.models import Product, CPmapping, User, PMmapping, Manufacturer
+from myadmin.util import get_center_coor, Manus
 
 
 # Create your views here.
@@ -13,6 +11,7 @@ def show_all(request):
     p_list = product.all()
     context = {'productlist': p_list}
     return render(request, 'myadmin/product/show.html', context)
+
 
 def show_details(request, p_id=0):
     product = Product.objects.get(p_id=p_id)
@@ -32,14 +31,44 @@ def show_details(request, p_id=0):
     try:
         for owner in owners:
             u_list.append(owner.c_id)
+        loc_dic = {}
+        m = folium.Map(width='100%', height='100%', location=get_center_coor(), zoom_start=8)
         for manufacturer in manufacturers:
             if manufacturer.m_pnode == 0:
-                m_list_0.append(Manufacturer.objects.get(m_id=manufacturer.m_id).m_name)
+                this_manu = Manufacturer.objects.get(m_id=manufacturer.m_id)
+                m_list_0.append(this_manu.m_name)
+                loc_dic[this_manu.m_name] = Manus(
+                    this_manu.m_name,
+                    this_manu.loc,
+                    this_manu.addtime,
+                    this_manu.modifytime,
+                    this_manu.description
+                )
             elif manufacturer.m_pnode != 0:
-                m_list.append(Manufacturer.objects.get(m_id=manufacturer.m_id).m_name)
+                this_manu = Manufacturer.objects.get(m_id=manufacturer.m_id)
+                m_list.append(this_manu.m_name)
                 parent_list.append(Manufacturer.objects.get(m_id=manufacturer.m_pnode).m_name)
+                loc_dic[this_manu.m_name] = Manus(
+                    this_manu.m_name,
+                    this_manu.loc,
+                    this_manu.addtime,
+                    this_manu.modifytime,
+                    this_manu.description
+                )
+        print(loc_dic)
+        for key in loc_dic.keys():
+            info = loc_dic[key]
+            '''<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" type="text/css" href="{% static 'bootstrap.min.css'%}"><link rel="stylesheet" type="text/css" href="{% static 'css/Sidebar.css'%}"></head>'''
+            iframe = folium.IFrame(
+                '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head><div class="card-body bg-light"><h4 class="card-text" style="margin-bottom:0px">%s</h4><p class="card-text" style="font-size:20px">Log:</br>%s</p><small class="text-muted" style="font-size:15px test-align:bottom">%s</small></div>'
+                % (info.name, info.description, info.change_msg))
+            popup = folium.Popup(iframe, min_width=400, max_width=400)
+            folium.Marker([info.lat, info.lon], tooltip='click here for more',
+                          popup=popup,
+                          icon=folium.Icon(color='purple')).add_to(m)
         users = User.objects.filter(u_id__in=u_list)
         m_info = []
+        m_html = m._repr_html_()
         for item in m_list_0:
             m_info.append(" name: %s, direct get from product constructor" % item)
         for i in range(len(m_list)):
@@ -47,11 +76,12 @@ def show_details(request, p_id=0):
         context = {'product': product,
                    'owners': users,
                    'm_info': m_info,
+                   'map': m_html,
                    'add_status': 1}
 
     except Exception as err:
         print('***************')
         print(err)
-        context = {'info': "Showing information for product %s failed, Please try again." % product.name,
+        context = {'info': "Showing information for product %s failed, Please try again." % product.p_name,
                    'add_status': 0}
     return render(request, 'myadmin/product/view.html', context)
